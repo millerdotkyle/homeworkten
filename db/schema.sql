8a1638e50492d0f1ca408ab07625b33e5eb32d58
millerdotkyle <kylenotfound@gmail.com>
2023-08-15 20:46:47 -0400

    added more logic
    -main prompt now lives inside callable function
    -add a role lives in if statement call to external logic

--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,23 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL,
-  dept_name VARCHAR(40) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  dept_name VARCHAR(40) NOT NULL UNIQUE,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-  id INT NOT NULL,
-  title VARCHAR(40) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(40) NOT NULL UNIQUE,
   department_id INT,
-  salary DECIMAL(7, 2),
+  salary DECIMAL(9, 2),
   FOREIGN KEY (department_id)
   REFERENCES departments(id),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE employees (
-  id INT NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const { async } = require('rxjs');
+const { resolve } = require('path');
+const { rejects } = require('assert');
+const { error } = require('console');
 
 // Connect to database
 const db = mysql.createConnection(
@@ -12,10 +16,8 @@ const db = mysql.createConnection(
     console.log(`Connected to the database.`)
   );
   
-
-
-
-inquirer
+init = () => {
+  inquirer
   .prompt([
     {
         type: 'list',
@@ -24,8 +26,86 @@ inquirer
         name: 'op'
     }
   ])
-  .then((answers) => {
-    console.log(answers);
+  .then((choice) => { 
+    
+    console.log(choice.op);
+
+    if(choice.op === "view all depts") {
+      db.query('SELECT * FROM departments', function(err, results) {
+        console.log(results);
+      });
+    };
+    if(choice.op === "view all roles") {
+      db.query('SELECT * FROM roles', function(err, results) {
+        console.log(results);
+      });
+    };
+    if(choice.op === "view all employees") {
+      db.query('SELECT * FROM employees', function(err, results) {
+        console.log(results);
+      });
+    };
+
+
+
+    if(choice.op === "add a dept") {
+      inquirer
+      .prompt([
+        {
+            type: 'input',
+            message: 'Enter name',
+            name: 'q'
+        }
+      ])
+      .then((choice) => { 
+        db.query(`INSERT INTO departments (deptartment_name) VALUES ('${choice.q}')`, function(err, results) {
+          console.log(err);
+          console.log(results);
+        });
+    })
+    };
+
+    if(choice.op === "add a role") {
+      addRole();
+      
+    };
 
 
   });
+
+};
+
+
+const addRole = () => {
+      db.query('SELECT * FROM departments', function(err, departments) {
+        if(err) {console.log(err)};
+        departments = departments.map((department) => {
+          return {name: department.name, value: department.id}
+        });
+      });
+      inquirer
+      .prompt([
+        {
+            type: 'input',
+            message: 'Enter role title',
+            name: 'title'
+        },
+        {
+          type: 'input',
+          message: 'What is the salary?',
+          name: 'salary' 
+        },
+        {
+          type: 'rawlist',
+          message: 'What department does this role belong to?',
+          choices: departments,
+          name: dept
+        }
+      ])
+      .then((choices) => { 
+        db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${choices.title}', '${choices.salry}', '${choices.dept}')`, function(err, results) {
+          if (err) {throw err};
+          console.log('ADDED!');
+          init();
+        });
+    })};
